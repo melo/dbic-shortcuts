7f9b01c44c416cb0e3b45b2c84353f679e0b037a
Pedro Melo <melo@simplicidade.org>
2010-01-29 18:26:22 +0000

    Tidy code and add comments
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/DBICx/Shortcuts.pm
+++ lib/DBICx/Shortcuts.pm
@@ -13,8 +13,8 @@ sub setup {
   die if $@;
   local $ENV{DBIC_NO_VERSION_CHECK} = 1;
   my $schema = $schema_class->connect;
-  
-  SOURCE: for my $source ($schema->sources) {
+
+SOURCE: for my $source ($schema->sources) {
     my $info = $schema->source($source)->source_info;
     next SOURCE if exists $info->{skip_shortcut} && $info->{skip_shortcut};
 
@@ -25,44 +25,52 @@ sub setup {
     }
     else {
       $method = $source;
-      $method =~ s/.+::(.+)$/$1/; ## deal with nested sources
+      $method =~ s/.+::(.+)$/$1/;              ## deal with nested sources
       $method =~ s/([a-z])([A-Z])/${1}_$2/g;
       $method = lc($method);
     }
-    
+
     croak("Shortcut failed, '$method' already defined in '$class', ")
       if $class->can($method);
-    
+
     no strict 'refs';
-    *{__PACKAGE__."::$method"} = sub {
+    *{__PACKAGE__ . "::$method"} = sub {
       my $rs = shift->schema->resultset($source);
-      
+
+      ## No arguments, return empty result set;
       return $rs unless @_;
+
+      ## first argument not a reference, assume find by PK
       return $rs->find(@_) if defined($_[0]) && !ref($_[0]);
+
+      ## first argument is a scalar ref, assume unique constraint name,
+      ## use find
       return $rs->find(@_[1 .. $#_], {key => ${$_[0]}})
         if defined($_[0]) && ref($_[0]) eq 'SCALAR';
+
+      ## otherwise, its a search
       return $rs->search(@_);
     };
   }
-  
+
   ## Enable set of schema shortcuts
   for my $meth (@methods) {
     no strict 'refs';
     *{__PACKAGE__ . "::$meth"} = sub { return shift->schema->$meth(@_) };
   }
 
-  $schemas{$class} = { class => $schema_class };
-  
+  $schemas{$class} = {class => $schema_class};
+
   return;
 }
 
 sub schema {
   my ($class) = @_;
-  
+
   croak("Class '$class' did not call 'setup()'")
     unless exists $schemas{$class};
 
-  my $info = $schemas{$class};
+  my $info   = $schemas{$class};
   my $schema = $info->{schema};
   return $schema if $schema;
 
@@ -109,10 +117,10 @@ DBICx::Shortcuts - Setup a class with shortcut methods to the sources of a DBIx:
   $book = S->books->create({ ... });
   
   # With first argument as a defined non-ref, passes @_ to find()
+  $book = S->books(42);  ## 42 is the PK of the book
   
   # With first argument as a ScalarRef, uses a unique constraint
   $book = S->book(\'isbn_key', '9123123432123');
-  $book = S->books(42);  ## 42 is the ID of the book
   
   # All other cases, calls search()
   $rs = S->books({title => { like => '%Perl%' }});
