ccb5c2e86770e05a5c02764a31153c92f7ae2162
Pedro Melo <melo@simplicidade.org>
2011-01-13 21:58:14 +0000

    Make tests use memory-based SQLite db
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- t/tlib/S3.pm
+++ t/tlib/S3.pm
@@ -1,21 +1,10 @@
 package S3;
 use parent 'DBICx::Shortcuts';
-use File::Temp qw( tmpnam );
 
 __PACKAGE__->setup('Schema');
 
-my $tmpname = tmpnam();
 sub connect_info {
-  my ($self, $extra) = @_;
-
-  my $name = $tmpname;
-  $name .= "-$extra" if $extra;
-  
-  return ("dbi:SQLite:$name") ;
+  return ("dbi:SQLite::memory:") ;
 }
-
-# END {
-#   unlink($tmpname) if $tmpname;
-# }
 
 1;
